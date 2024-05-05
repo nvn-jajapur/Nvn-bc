11a2cbdd38ac48b2ec391d4aefe6ee4a065fd890
unknown <naveen.jajapur-1@ou.edu>
2024-05-04 22:17:40 -0500

    Updated ABI and Contract Address

--- utils/connections.js
+++ utils/connections.js
@@ -1,4 +1,4 @@
 import abi from "./compData.json";
 
-export const contractAddress = "0x516Ea3E181bF3000E3db61D16dF9528611193614";
+export const contractAddress = "0x0BF4bCeF6AC1dca8EBCE67DF661CC7770A13e6f8";
 export const contractABI = abi.abi;
